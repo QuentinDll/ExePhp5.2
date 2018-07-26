8197d2a322b031a1fe1f63e8dd99ec19c2a714cd
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:27:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $gender = 'Homme';
+
+      if ($gender != 'Homme'){
+        echo "C'est une développeuse !!!";
+
+      }else{
+        echo "C'est un développeur !!!";
+      }
+       ?>
+    </p>
+  </body>
+</html>
